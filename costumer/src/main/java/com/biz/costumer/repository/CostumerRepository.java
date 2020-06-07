1e5c3372fbf29f4dd3aa35762cb75b0fc10a5659
Ricardo Brasil <anribrasil@gmail.com>
2020-06-06 21:18:05 -0300

    costumer repository

--- costumer/src/main/java/com/biz/costumer/repository/CostumerRepository.java
+++ costumer/src/main/java/com/biz/costumer/repository/CostumerRepository.java
@@ -1,7 +1,9 @@
 package com.biz.costumer.repository;
 
 import com.biz.costumer.model.Costumer;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CostumerRepository extends Repository<Costumer, Long> {
+@Repository
+public interface CostumerRepository extends JpaRepository<Costumer, Long> {
 }

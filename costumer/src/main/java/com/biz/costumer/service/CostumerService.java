a70f5836f931a335a01fc67f5968443c1adb9f1a
Ricardo Brasil <anribrasil@gmail.com>
2020-06-06 21:18:19 -0300

    create method on service

--- costumer/src/main/java/com/biz/costumer/service/CostumerService.java
+++ costumer/src/main/java/com/biz/costumer/service/CostumerService.java
@@ -1,4 +1,18 @@
 package com.biz.costumer.service;
 
+import com.biz.costumer.model.Costumer;
+import com.biz.costumer.payload.CostumerPayload;
+import com.biz.costumer.repository.CostumerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class CostumerService {
+
+    private final CostumerRepository costumerRepository;
+
+    public void create(Costumer costumer) {
+        costumerRepository.save(costumer);
+    }
 }

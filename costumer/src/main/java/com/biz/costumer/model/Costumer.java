8780b3584e22b8ad77ce72ea0b4c7ef046452e12
Ricardo Brasil <anribrasil@gmail.com>
2020-06-06 21:12:00 -0300

    add costumer model

--- costumer/src/main/java/com/biz/costumer/model/Costumer.java
+++ costumer/src/main/java/com/biz/costumer/model/Costumer.java
@@ -1,4 +1,34 @@
 package com.biz.costumer.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "costumer")
 public class Costumer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Size(min=2, max=45)
+    private String name;
+
+    @NotEmpty
+    private String cpf;
+
+    @NotEmpty
+    @Email
+    private String email;
 }

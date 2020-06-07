66d17a47fd838a71801c2563c331731c37a2997a
Ricardo Brasil <anribrasil@gmail.com>
2020-06-06 21:13:09 -0300

    add costumer repository

---
+++ costumer/src/main/java/com/biz/costumer/repository/CostumerRepository.java
@@ -0,0 +1,7 @@
+package com.biz.costumer.repository;
+
+import com.biz.costumer.model.Costumer;
+import org.springframework.data.repository.Repository;
+
+public interface CostumerRepository extends Repository<Costumer, Long> {
+}
